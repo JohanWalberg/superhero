b4a094e422d5d03954fc060dfab2238d407f773c
Johan Walberg <Johanwalberg@outlook.com>
2018-02-11 10:18:35 +0000

    Added Random Avatar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,31 @@ class HomeController < ApplicationController
    @corp_power = Faker::Company.bs
    @corp_title = Faker::Name.title
    @corp_power = @corp_power.titleize
+   
+   @rand_num = rand(1..1000)
+   @set_num = rand(1..4)
+   @bg = rand(1..2)
+   
+   
+   # Randomly pick our avatar set!
+   if @set_num == 1
+       @set = 'set1'
+    elsif @set_num == 2
+        @set = 'set2'
+    elsif @set_num == 3
+        @set = 'set3'
+    else
+        @set = 'set4'
+   end
+   
+   # Randomly pick our background!
+   if @bg == 1
+        @background = 'bg1'
+    else
+        @background = 'bg2'
+   end
+   
+   @avatar = Faker::Avatar.image(@rand_num, "300x300", "png", @set, @background)
   end
   
   def index
